2d187a6c4910584b33900dee680faa19640c9010
Helmut Januschka <helmut@januschka.com>
2016-10-02 19:54:21 +0200

    up

--- run.rb
+++ run.rb
@@ -3,7 +3,7 @@ require 'stm_api'
 
 
 class MealOMatic
-	attr_accessor :device, :baud, :cents_per_signal, :credit, :donate_rate, :serial_port, :userhash
+	attr_accessor :device, :baud, :cents_per_signal, :credit, :donate_rate, :serial_port, :userhash, :thread, :mutex
 	def initialize()
 		@device = ARGV[1]
 		@baud = 9600
@@ -12,18 +12,34 @@ class MealOMatic
 		@donate_rate = 40
 		@userhash = ARGV[0];
 		@serial_port = SerialPort.new @device, @baud, 8, 1, SerialPort::NONE
+		@mutex = Mutex.new
 		
+		self.thread = []
+		self.thread << Thread.new do
+			puts 'Donation thread started'
+			donation_thread
+		end
+		
+		
+		puts "initialize Coin Acceptor Donation System!!! Lets make the world a better place!"
 	end
 
-
-	def listen_for_coins
+	def donation_thread
 		loop do
-			if @credit > @donate_rate
+			@mutex.synchronize do
 				donate_now
 			end
-			#puts "Current Un-Donated Credits: #{@credit}"
-		  r = serial_port.read(1)
-		  @credit += cents_per_signal
+			puts "Credit Left: #{@credit}"
+			sleep 10
+		end
+	end
+	def listen_for_coins
+		#@credit = 86
+		loop do
+			  r = serial_port.read(1)
+					@mutex.synchronize do
+		    	@credit += cents_per_signal
+				end
 			
 		end
 	end
@@ -34,10 +50,14 @@ class MealOMatic
 		for i in 1..donates.floor
 			@credit -= @donate_rate
 			burger << "🍔"
+			api.donate(amount: (@donate_rate/100).to_s)
 		end
-		puts "Donated: #{burger.length} that is:"
-		puts burger.join(" ")
-		puts "Credit Left: #{@credit}"
+		spent = (burger.length*@donate_rate).to_f/100
+		if burger.length > 0
+			puts "Donated #{burger.length} meals, worth #{spent} EUR - these are:"
+			puts burger.join(" ")
+		end
+		
 		#result = api.donate(amount: '0.4')
 	end
 end
